cbf1fc6d72cf9b3491fc465b5a3e87e324f55de7
kamal7643 <67092345+kamal7643@users.noreply.github.com>
2022-03-31 16:03:33 +0530

    Update request_api.py

--- routes/request_api.py
+++ routes/request_api.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, jsonify, request, abort, Response
 from datetime import datetime, timedelta
 from supabase_client import Client
+import requests
 
 
 
 REQUEST_API = Blueprint('request_api', __name__)
+ip_url = "https://ifconfig.co/ip"
+
 
 url= 'https://tlagwlowvabyydvzipai.supabase.co'
 key= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsYWd3bG93dmFieXlkdnppcGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY3NjY2MDksImV4cCI6MTk2MjM0MjYwOX0.jicG5Cyvw7wW7Sw_lkHQVqv6cEogH0YJzR_GrL7HEXM'
@@ -14,7 +17,14 @@ supabase = Client(
 )
 
 
-
+def make_return(response):
+	ip = requests.get(ip_url).text
+	data = {
+		"source_ip":ip,
+		"source_name":"delivery Service ",
+		"response":response
+	}
+	return jsonify(data)
 
 def get_blueprint():
     """Return the blueprint for the main app module"""
@@ -26,20 +36,20 @@ def get_blueprint():
 async def handle_all_delivery():
     if request.method=="GET":
         error , data = await(supabase.table("delivery").select("*").query())
-        return jsonify(data)
+        return make_return(data)
     elif request.method=="POST":
         error, result = await(supabase.table("delivery").insert([request.get_json()]))
-        return jsonify(result)
+        return make_return(result)
 
 @REQUEST_API.route('/loyalty',methods=['GET'])
 async def get_loyalty():
     error, data = await(supabase.table("loyalty").select("*").query())
-    return jsonify(data)
+    return make_return(data)
 
 @REQUEST_API.route("/loyalty/<uid>", methods=["PUT", "GET"])
 async def handle_loyalty(uid):
     if request.method == "GET":
         error , data = await(supabase.table("loyalty").select("*").eq('username',uid).query())
-        return jsonify(data)
+        return make_return(data)
     else:
-        return jsonify("ok")
+        return make_return("ok")
